ee89210fec747ea149f2beddb98e66c14a663597
agrahariashish <agrahariashish120@gmail.com>
2021-03-25 10:01:05 +0530

    First commit

---
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -0,0 +1,17 @@
+package com.lti.view;
+
+import com.lti.model.Employee;
+
+public class EmpView {
+
+	public static void main(String[] args) {
+		Employee emp=new Employee();
+	    emp.setEmpId(1001);
+	    emp.setEmpName("Ashish");
+	    System.out.println("Id:"+emp.getEmpId()+"Name:"+emp.getEmpName());
+		
+		}
+		
+
+	}
+
