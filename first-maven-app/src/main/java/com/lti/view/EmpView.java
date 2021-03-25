6d31d4460dd62a440249912437b2c7be93849b05
agrahariashish <agrahariashish120@gmail.com>
2021-03-25 12:37:53 +0530

    second commit

--- first-maven-app/src/main/java/com/lti/view/EmpView.java
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -7,7 +7,7 @@ public class EmpView {
 	public static void main(String[] args) {
 		Employee emp=new Employee();
 	    emp.setEmpId(1001);
-	    emp.setEmpName("Ashish");
+	    emp.setEmpName("ash");
 	    System.out.println("Id:"+emp.getEmpId()+"Name:"+emp.getEmpName());
 		
 		}
